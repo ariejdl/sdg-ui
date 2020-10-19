62d6785da63c680bdaa0bf348e03b6263daae919
arie <arie@users-MBP-3.lan>
2020-10-19 19:00:25 +0100

    enhance calculator with recursion

--- static/calculator.js
+++ static/calculator.js
@@ -3,21 +3,26 @@
  * like excel, with a few differences
 
 - ‘automatic’ or ‘manual’ node, automatic update every time, manual updated trigger a cascade when updated, similar to excel function vs. excel macro;
-- allow cycle, max repeat of throttle; time executions, make manual if slow...assumption is that it behaves like excel
+- allow cycle, max repeat of throttle; time evaluations, make manual if slow...assumption is that it behaves like excel
 
 */
 
+const MAX_RECURSION = 10;
+
 const invokeNode = (data, successors) => {
 
-  return new Promise((resolve, reject) => {
-    console.log(data.kind)
-    resolve();
-  });
 
-  if (data.kind === "grid") {
+  if (data.kind === "notebook") {
+    // execute each cell
+  } else if (data.kind === "grid") {
   } else if (data.kind === "tree") {
     
   }
+
+  return new Promise((resolve, reject) => {
+    console.log('invoke: ' + data.kind)
+    resolve();
+  });  
   
 }
 
@@ -25,26 +30,47 @@ export class Calculator {
 
   constructor(cy) {
     this._cy = cy;
-    this._evalIncrement = 1;
+    this._currentEvalIncrement = 1;
   }
 
-  evalNode(id) {
-    // - throw exception if not found
+  evalNode(id, isManual) {
+    this._evalNode(id, ++this._currentEvalIncrement, isManual);
+  }
+
+  _evalNode(id, evalId, isManual) {
+
+    if (evalId !== this._currentEvalIncrement) {
+      // stop, no further computation, only one calculation at a time
+      console.warn(`only one computation allowed at a time, stopping: ${this._currentEvalIncrement}`)
+      return;
+    }
+    
+    // throw exception if not found
     const n = this._cy.$(id)
     if (n.length !== 1) {
       throw `${id} not found`;
     }
 
     const data = n.data();
+    const isAutoNode = (data['run_auto'] || 'auto') === 'auto';
 
-    // TODO: check recursion in scratch
-    const scratch = n.scratch("_exec") || {};
-    scratch.recursion = scratch.recursion || 0;
+    // check if auto/manual/undefined, and evaluation/invocation if manual    
+    if (!isAutoNode && !isManual) {
+      console.log('manual node not evaluated as evaluation is auto')
+      return;
+    }
+
+    // don't check for cycles, but do track evaluation of nodes
+    const scratch = n.scratch("_eval") || {};
+    scratch['call_count'] = (scratch['call_count'] || 0) + 1;
+    n.scratch("_eval", scratch)
+
+    if (scratch['call_count'] > MAX_RECURSION) {
+      console.warn(`maximum recursion reached for ${id}, stopping`)
+      return;
+    }
     
     console.log('eval', id)
-    console.log('scratch', scratch)
-
-    // - don't check for cycles, but do track execution of nodes...
     
     // - create a variable 'evaluation-id', to track spread of evaluation
 
@@ -54,18 +80,17 @@ export class Calculator {
     // .... e.g. emit custom events/invocations per cell,
     // thus decompose a notebook into a chain of promises with sideffects
     // -> ? getInvokationFunction() ...
-    // ... remember this may be interrupted too
-    // ... remember it's probably best to stop notebooks creating invocation loops of themselves
+    // ... remember: this may be interrupted too
+    // ... remember: it's probably best to stop notebooks creating invocation loops of themselves
 
     // 1) get outward
 
     const successors = n.successors().filter(o => o.isNode());
 
-    // may want to time execution, and use a promise here after finishes
+    // may want to time evaluation, and use a promise here after finishes
     const start = Date.now();
-    //n.trigger('update')
 
-    const ret = invokeNode(data, successors);
+    const ret = invokeNode(data, successors, evalId);
 
     if (ret === undefined) {
       return;
@@ -80,9 +105,8 @@ export class Calculator {
       const outgoers = n.outgoers().filter(o => o.isNode());
 
       outgoers.forEach((obj) => {
-        console.log(obj)
         const id = obj.data()["id"];
-        //this.evalNode("#" + id);
+        this._evalNode("#" + id, evalId, isManual);
       });
 
       
